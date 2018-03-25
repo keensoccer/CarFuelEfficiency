460f136d0c12b47aa60d97e90827e5567f795e60
Achintya G <achintya.9upt@gmail.com>
2018-03-25 12:11:02 -0700

    Add project files.

---
+++ CarFuelEfficiency/Cars.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CarFuelEfficiency
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public double EngDisplay { get; set; }
+        public int Cylinders { get; set; }
+        public int CityFE { get; set; }
+        public int HighwayFE { get; set; }
+        public int CombinedFE { get; set; }
+
+        public static IEnumerable<Car> CarFileToListOfCars(string fileName)
+        {
+            var carDataLines = File.ReadAllLines(fileName);
+
+            var cars = new List<Car>();
+
+            for (int i = 1; i < carDataLines.Length; i++)
+            {
+                var carProperties = carDataLines[i].Split(',');
+
+                var car = new Car();
+                car.Year = int.Parse(carProperties[0]);
+                car.Manufacturer = carProperties[1];
+                car.Model = carProperties[2];
+                car.EngDisplay = double.Parse(carProperties[3]);
+                car.Cylinders = int.Parse(carProperties[4]);
+                car.CityFE = int.Parse(carProperties[5]);
+                car.HighwayFE = int.Parse(carProperties[6]);
+                car.CombinedFE = int.Parse(carProperties[7]);
+
+                cars.Add(car);
+
+            }
+
+            return cars;
+        }
+
+    }
+
+
+}
---
+++ CarFuelEfficiency/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CarFuelEfficiency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manufacturers = File.ReadAllLines("manufacturers.csv.txt");
+
+            var cars = Car.CarFileToListOfCars("fuel.csv");
+
+            PrintTopNCarsByFuelEfficiences(cars, 6);
+            Console.WriteLine("*****************************");
+            PrintCarStatistics(cars);
+
+            Console.ReadLine();
+        }
+
+        public static void PrintTopNCarsByFuelEfficiences(IEnumerable<Car> cars, int n)
+        {
+            var query =
+                from car in cars
+                orderby car.CombinedFE descending
+                select car;
+
+            Console.WriteLine("The top 6 most fuel efficient cars:");
+            foreach (var car in query.Take(n))
+            {
+                Console.WriteLine($"\tThe {car.Model} has a combined fuel efficiency of {car.CombinedFE}");
+            }
+        }
+
+        public static void PrintTopNCarsByFuelEfficiencesByManufacturer(IEnumerable<Car> cars, string manufacturer, int n)
+        {
+
+        }
+
+        public static void PrintCarStatistics(IEnumerable<Car> cars)
+        {
+            var total = 0;
+            var min = Int32.MaxValue;
+            var max = Int32.MinValue;
+            var count = 0-1;
+            foreach (var car in cars)
+            {
+                total += car.CombinedFE;
+                if(min > car.CombinedFE)
+                {
+                    min = car.CombinedFE;
+                }
+                if (max < car.CombinedFE)
+                {
+                    max = car.CombinedFE;
+                }
+                count++;
+            }
+
+            Console.WriteLine("Statistics about the cars:");
+
+            Console.WriteLine($"\tThe total combined fuel efficiency is {total}.\n\tThe maximum combined fuel efficency is {max}. " +
+                              $"\n\tThe minimum combined fuel efficincy is {min}.\n\tThe total number of cars is {count}.");
+        }
+
+        public static void PrintCarStatisticsByManufacturer(IEnumerable<Car> cars, string manufacturer)
+        {
+            //avg, max, min, count
+        }
+
+    }
+}
